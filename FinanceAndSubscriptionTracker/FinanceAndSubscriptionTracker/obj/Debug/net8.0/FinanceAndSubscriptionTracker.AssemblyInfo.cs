28baf1d82f2a2bd6b1aebf6b233ac9881076eebd
Im_uBBko <165716577+uBBkocz@users.noreply.github.com>
2026-02-12 22:00:23 +0100

    Learning how to use GitHub with Visual Studio :D

--- FinanceAndSubscriptionTracker/FinanceAndSubscriptionTracker/obj/Debug/net8.0/FinanceAndSubscriptionTracker.AssemblyInfo.cs
+++ FinanceAndSubscriptionTracker/FinanceAndSubscriptionTracker/obj/Debug/net8.0/FinanceAndSubscriptionTracker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinanceAndSubscriptionTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b75a345eab6ea7b13e512279c3eaaadb012496e")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinanceAndSubscriptionTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinanceAndSubscriptionTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
